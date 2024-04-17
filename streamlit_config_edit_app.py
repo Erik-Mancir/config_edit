5799c7663627c1b37a53b021cdfdf873898caad1
ErikMancir <99759120+ErikMancir@users.noreply.github.com>
2024-04-17 16:30:29 +0200

    Update streamlit_config_edit_app.py

--- streamlit_config_edit_app.py
+++ streamlit_config_edit_app.py
@@ -5,14 +5,14 @@ import requests
 import pandas as pd
 
 # Write directly to the app
-st.title("Customize Your Smoothie :cup_with_straw:")
+st.title("Config tables editor :lab_coat:")
 st.write(
-    """Choose the fruits you want in your custom smoothie.
+    """Choose the table you want to edit.
     """
 )
 
 name_on_order = st.text_input('Name on Smoothie')
-st.write('The name on your Smoothie will be:', name_on_order)
+#st.write('The name on your Smoothie will be:', name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
